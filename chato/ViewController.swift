6ff682503c13873dc8cb8d6761bdcab26d419801
diegobc <diegobc@gmail.com>
2016-08-04 13:12:44 +0200

    Initial Commit

---
+++ chato/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chato
+//
+//  Created by dguimaraesdebr on 04/08/16.
+//  Copyright © 2016 dc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
